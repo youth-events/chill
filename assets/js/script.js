cc4813243b4852bd278d5caf2fd37cc472c8f88e
youth-events <youth.vn@yandex.ru>
2023-07-11 00:58:18 +0300

    download_11_07_2023_9

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,34 @@
 // // push
-// function start () {
-// 	Push.create("Уведомление от chill-vn", {
-// 		body: "Новое мероприятие 16_07_2023",
-// 		icon: 'https://youth-events.github.io/chill/images/coffee_icon.png',
-// 		timeout: 4000
-// 	});
-// }
-// document.querySelector('#btn').addEventListener('click', () => start());
+function start () {
+	Push.create("Уведомление от chill-vn", {
+		body: "Новое мероприятие 16_07_2023",
+		icon: 'https://youth-events.github.io/chill/images/coffee_icon.png',
+		timeout: 4000
+	});
+}
+document.querySelector('#btn').addEventListener('click', () => start());
 
-//
-//
-// function notifyMe () {
-//   var notification = new Notification ("Уведомление от chill-vn", {
-//     tag: "ache-mail",
-//     body: "Новое мероприятие намечается",
-//     icon: "https://youth-events.github.io/chill/images/coffee_icon.png"
-//   });
-// }
-//
-// function notifSet () {
-//   if (!("Notification" in window))
-//   alert ("Ваш браузер не поддерживает уведомления!");
-//   else if (Notification.permission === "granted")
-//   setTimeout(notifyMe, 2000);
-//   else if (Notification.permission !== "denied") {
-//     Notification.requestPermission (function (permission) {
-//       if (!('permission' in Notification))
-//       Notification.permission = permission;
-//       if (permission == "granted"),
-//       setTimeout(notifyMe, 2000);
-//     });
-//   }
-// }
+
+
 function notifyMe () {
-		var notification = new Notification ("Все еще работаешь?", {
-			tag : "ache-mail",
-			body : "Пора сделать паузу и отдохнуть",
-			icon : "https://itproger.com/img/notify.png"
-		});
-	}
+  var notification = new Notification ("Уведомление от chill-vn", {
+    tag : "ache-mail",
+    body : "Новое мероприятие намечается",
+    icon : "https://youth-events.github.io/chill/images/coffee_icon.png"
+  });
+}
 
-	function notifSet () {
-		if (!("Notification" in window))
-			alert ("Ваш браузер не поддерживает уведомления.");
-		else if (Notification.permission === "granted")
-			setTimeout(notifyMe, 2000);
-		else if (Notification.permission !== "denied") {
-			Notification.requestPermission (function (permission) {
-				if (!('permission' in Notification))
-					Notification.permission = permission;
-				if (permission === "granted")
-					setTimeout(notifyMe, 2000);
-			});
-		}
-	}
+function notifSet () {
+  if (!("Notification" in window))
+  alert ("Ваш браузер не поддерживает уведомления!");
+  else if (Notification.permission === "granted")
+  setTimeout(notifyMe, 2000);
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission (function (permission) {
+      if (!('permission' in Notification))
+      Notification.permission = permission;
+      if (permission === "granted"),
+      setTimeout(notifyMe, 2000);
+    });
+  }
+}
