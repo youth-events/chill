64b2e100df1fa4235dd14f4c4671fea77046fb66
youth-events <youth.vn@yandex.ru>
2023-07-11 00:48:05 +0300

    downllaod_11_07_2023_7

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,15 +18,13 @@ function notifyMe () {
   });
 }
 
-
-
-function notifset () {
+function notifSet () {
   if (!("Notification" in window))
   alert ("Ваш браузер не поддерживает уведомления!");
   else if (Notification.permission === "granted")
   setTimeout(notifyMe, 2000);
   else if (Notification.permission !== "denied") {
-    Notification.requestPermission (function (permission)) {
+    Notification.requestPermission (function (permission) {
       if (!('permission' in Notification))
       Notification.permission = permission;
       if (permission == "granted"),
