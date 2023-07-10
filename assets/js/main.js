15a1608e758993b9185d2ce65c7287e23ec8ab3a
youth-events <youth.vn@yandex.ru>
2023-07-10 22:41:14 +0300

    download_10_07_2023_9

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 
+
 (function($) {
 
 	var	$window = $(window),
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// push
+function start() {
+	Push.create("Уведомление от chill-vn", {
+		body: "Новое мероприятие 16_07_2023",
+		icon: 'https://youth-events.github.io/chill/images/coffee_icon.png',
+		timeout: 4000
+	});
+}
+document.querySelector('#btn').addEventListener('click', () => start());
