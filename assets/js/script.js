b56491fbbf540cece6458ce8a00d35df4b8c962d
youth-events <youth.vn@yandex.ru>
2023-07-11 00:35:33 +0300

    download_11_07_2023_5

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,28 +9,28 @@ function start () {
 document.querySelector('#btn').addEventListener('click', () => start());
 
 
-//
-// function notifyMe () {
-//   var notification = new Notification ("Уведомление от chill-vn", {
-//     tag: "ache-mail",
-//     body: "Новое мероприятие намечается",
-//     icon: "https://youth-events.github.io/chill/images/coffee_icon.png"
-//   });
-// }
-//
-//
-//
-// function notifset () {
-//   if (!("Notification" in window))
-//   alert ("Ваш браузер не поддерживает уведомления!");
-//   else if (Notification.permission === "granted")
-//   setTimeout(notifyMe, 2000);
-//   else if (Notification.permission !== "denied") {
-//     Notification.requestPermission (function (permission)) {
-//       if (!('permission' in Notification))
-//       Notification.permission = permission;
-//       if (permission == "granted"),
-//       setTimeout(notifyMe, 2000);
-//     });
-//   }
-// }
+
+function notifyMe () {
+  var notification = new Notification ("Уведомление от chill-vn", {
+    tag: "ache-mail",
+    body: "Новое мероприятие намечается",
+    icon: "https://youth-events.github.io/chill/images/coffee_icon.png"
+  });
+}
+
+
+
+function notifset () {
+  if (!("Notification" in window))
+  alert ("Ваш браузер не поддерживает уведомления!");
+  else if (Notification.permission === "granted")
+  setTimeout(notifyMe, 2000);
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission (function (permission)) {
+      if (!('permission' in Notification))
+      Notification.permission = permission;
+      if (permission == "granted"),
+      setTimeout(notifyMe, 2000);
+    });
+  }
+}
